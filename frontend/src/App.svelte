b3bb1b9b18b39bfaf822d09ac257b51e1d29f8ad
Nabuki <shichiromail@gmail.com>
2024-04-09 16:52:52 +0300

    add multichoise dropdown

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,14 +1,22 @@
 <script>
   import TextImageInput from "./components/TextImageInput.svelte";
   import LimitNumberInput from "./components/LimitNumberInput.svelte";
+  import MultiChoiceDropdown from "./components/MultiChoiceDropdown.svelte";
 
   function handleUpdate(event) {
     console.log(event.detail);
   }
+
+  let options = [
+    { id: 1, name: "Option 1", selected: false },
+    { id: 2, name: "Option 2", selected: false },
+    { id: 3, name: "Option 3", selected: false },
+  ];
 </script>
 
 <main>
   <TextImageInput />
+  <MultiChoiceDropdown {options} />
   <LimitNumberInput on:update={handleUpdate} />
 </main>
 
