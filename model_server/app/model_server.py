0d970b753b0b04c79649073f3c2d31c00fcdeec2
Evgenij Ivankin <e.ivankin@innopolis.university>
2024-04-11 03:11:04 +0300

    feat: using models in progress

--- model_server/app/model_server.py
+++ model_server/app/model_server.py
@@ -3,21 +3,31 @@ from io import BytesIO
 from ray import serve
 from starlette.requests import Request
 from PIL import Image
+from sentence_transformers import SentenceTransformer
 
 
 @serve.deployment
-class EmbeddingModel:
-    # TODO: decide, how we gonna split text/img embedding, maybe 2 different models
+class TextEmbeddingModel:
     def __init__(self):
-        self.text_model = ...
-        self.image_model = ...
+        self.text_model = SentenceTransformer(
+            "sentence-transformers/clip-ViT-B-32-multilingual-v1"
+        )
 
     async def __call__(self, starlette_request: Request) -> list[float]:
-        # image_payload_bytes = await starlette_request.body()
-        # pil_image = Image.open(BytesIO(image_payload_bytes))
-        # TODO
-        return [1] * 512
+        payload = await starlette_request.json()
+        return self.text_model.encode(payload["text_query"]).tolist()
 
 
-embedding_model = EmbeddingModel.bind()
+@serve.deployment
+class ImageEmbeddingModel:
+    def __init__(self):
+        self.image_model = SentenceTransformer("clip-ViT-B-32")
 
+    async def __call__(self, starlette_request: Request) -> list[float]:
+        image_payload_bytes = await starlette_request.body()
+        pil_image = Image.open(BytesIO(image_payload_bytes))
+        return self.image_model.encode(pil_image).tolist()
+
+
+image_model = ImageEmbeddingModel.options(route_prefix="/image-embedding").bind()
+text_model = TextEmbeddingModel.options(route_prefix="/text-embedding").bind()
